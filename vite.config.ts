c148c7c8ebd740bb9bfa7d2349e64d593ec2cfbc
Charan <veldurthysaicharan5@gmail.com>
2025-07-18 14:00:42 -0700

    Final commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Sales-Forecasting-Using-Machine-Learning/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
